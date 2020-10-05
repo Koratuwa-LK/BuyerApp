2df0ab66bdbb715a04cfc11e632298897a53ea40
kodda96 <mailtonipuna@yahoo.com>
2020-10-05 11:06:55 +0530

    add reviews

---
+++ navigation/OrderNavigator.js
@@ -0,0 +1,36 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+
+import OrderScreen from "../screens/OrderScreen";
+import MarketScreen from "../screens/MarketScreen";
+import HomeScreen from "../screens/HomeScreen";
+import TransportScreen from "../screens/TransportScreen";
+import landingScreen from "../screens/landingscreen";
+import listScreen from "../screens/listscreen";
+import mapScreen from "../screens/mapscreen";
+import bookingscreen from "../screens/bookingscreen";
+import LoginScreen from "../screens/LoginScreen";
+import SignUpScreen from "../screens/SignUpScreen";
+import LoadingScreen from "../screens/LoadingScreen";
+import BookinghistoryScreen from "../screens/bookinghistoryscreen";
+import OrderHistoryScreen from "../screens/OrderHistoryScreen";
+import ReviewScreen from "../screens/ReviewScreen";
+
+const OrderNavigator = createStackNavigator({
+  Loading: LoadingScreen,
+  Login: LoginScreen,
+  SignUp: SignUpScreen,
+  Home: HomeScreen,
+  Market: MarketScreen,
+  Order: OrderScreen,
+  Reviews: ReviewScreen,
+  Transport: TransportScreen,
+  OrderHistory: OrderHistoryScreen,
+  Booking: landingScreen,
+  History: BookinghistoryScreen,
+  List: listScreen,
+  Mapview: mapScreen,
+  Checkout: bookingscreen,
+});
+
+export default createAppContainer(OrderNavigator);
