4ac57bf77d7ae71a87e2a9d151dbc2d30c876cc2
shnrndk <shnrndk@gmail.com>
2020-10-11 16:30:35 +0530

    Price Matching Notification Feature added

--- screens/NotificationScreen.js
+++ screens/NotificationScreen.js
@@ -1,20 +1,92 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
+import { Card, Button, Title, Paragraph, Text } from 'react-native-paper';
+import firebase from "firebase";
 
-export default class NotificationScreen extends React.Component {
-  render() {
-    return (
+
+import { db } from "../database/db.js";
+
+
+export default function NotificationScreen() {
+
+  const [notification, setNotification] = useState({ notificationList: [] })
+
+  useEffect(() => {
+
+    const BuyerId = firebase.auth().currentUser.uid;
+
+  
+   let subscriptionlist;
+    let tempsubscriptionList = [];
+    let stockslist;
+    let tempstockList = [];
+
+    const subscriptions = db.ref("sub").orderByChild("BuyerId").equalTo(BuyerId);
+    subscriptions.on('value', snapshot => {
+      const subscriptionlist = snapshot.val();
+      for (let key in subscriptionlist) {
+        tempsubscriptionList.push(subscriptionlist[key])
+      }
+    })
+
+    const stocks = db.ref("Stocks");
+    stocks.on('value', snapshot => {
+      const stockslist = snapshot.val();
+      for (let key in stockslist) {
+        tempstockList.push(stockslist[key])
+      }
+    })
+
+    const tempNotificationList = []
+
+    tempsubscriptionList.map(sub => {
+      tempstockList.map(stock => {
+        if (sub['maxPrice'] > stock['price'] && sub['minPrice'] < stock['price'] && sub['crop'] == stock['crop'] && sub['quantity'] <= stock['quantity']) {
+          tempNotificationList.push(stock)
+          console.log(tempNotificationList)
+        }
+      })
+    })
+
+    setNotification({ notificationList: tempNotificationList })
+
+  }, [])
+
+  return (
+    <ScrollView style={styles.scrollView}>
       <View style={styles.container}>
-        <Text> NotificationHome </Text>
+
+        {
+          notification.notificationList.map(val => {
+            return (
+              <Card style={styles.card} key={val.timestamp}>
+                <Card.Content>
+                  <Title>{val.crop}</Title>
+                  <Paragraph>Farmer Name : {val.name}</Paragraph>
+                  <Paragraph>Eco Center : {val.economicCenter}</Paragraph>
+                  <Paragraph>Farmer Contact No : {val.phoneNum}</Paragraph>
+                  <Paragraph>Price : {val.price}</Paragraph>
+                  <Paragraph>Quantity : {val.quantity}</Paragraph>
+                </Card.Content>
+                <Card.Actions style={{ alignSelf: "flex-end" }}>
+                  <Button onPress={() => console.log("Sahan")}>Ignore</Button>
+                </Card.Actions>
+              </Card>
+            )
+          })
+        }
       </View>
-    );
-  }
+    </ScrollView>
+  );
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+
   },
+  card: {
+    marginHorizontal: 10,
+    marginVertical: 4
+  }
 });
